9dd2c4fd8c03c22db5f73545a38e8924fddf6fde
gcc2ge <1414786333@qq.com>
2018-05-21 18:32:27 +0800

    init

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var q = require('./lib/taskqueue');
-var txSender=require('./lib/txSender');
+var txSender = require('./lib/txSender');
 
 function init(info) {
     txSender.init(info);
 }
+
 async function batchTransfer(sendList) {
     for (let i = 0; i < sendList.length; i++) {
         let send = sendList[i];
         let addr = send["to"];
+        console.info(`addr is ${addr}`)
         let amount = send["amount"];
-        q.txQ.push({to:addr,amount:amount})
+        q.txQ.push({to: addr, amount: amount});
     }
     // q.on('drain', function (){})
 }
