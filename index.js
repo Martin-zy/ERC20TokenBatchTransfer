4cef5c852ee4acbc9197f1d9d0c82dfc6b34bfd0
gcc2ge <1414786333@qq.com>
2018-05-24 23:24:17 +0800

    upgrade

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var q = require('./lib/taskqueue');
 var txSender = require('./lib/txSender');
+var txEvent = require('./lib/taskqueue').txEvent;
 var sleep = require('./lib/sleep');
 
 function init(info) {
     txSender.init(info);
 }
 
+var recivedTxCount = 0;
+var sentTxCount = 0;
+
+txEvent.on('tx_success', function (tx) {
+    recivedTxCount = recivedTxCount + 1;
+});
+
+
+txEvent.on('tx_fail', function (tx) {
+    recivedTxCount = recivedTxCount + 1;
+});
+
 async function batchTransfer(sendList) {
-    for (let i = 0; i < sendList.length; i++) {
-        let send = sendList[i];
-        let addr = send["to"];
-        let amount = send["amount"];
-        q.txQ.push({to: addr, amount: amount});
-        if (i % 5 == 0) {
-            await sleep(5 * 60 * 1000);
+    let total = sendList.length;
+    let futureTxCount = 0;
+    var myInterval = setInterval(function () {
+        futureTxCount = sentTxCount - recivedTxCount;
+        if (sentTxCount < total && futureTxCount < 5) {
+            let send = sendList[sentTxCount];
+            let addr = send["to"];
+            let amount = send["amount"];
+            q.txQ.push({to: addr, amount: amount});
+            sentTxCount = sentTxCount + 1;
         }
-    }
+        if (sentTxCount >= total) {
+            clearTimeout(myInterval);
+        }
+    }, 1000);
+
     // q.on('drain', function (){})
 }
 
